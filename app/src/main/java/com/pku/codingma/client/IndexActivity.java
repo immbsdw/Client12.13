5631412759bcdf044c68742a0cfb2c52c85dcf73
BROBIRD <brobirdcn@gmail.com>
2017-12-27 03:45:38 +0800

    2017年12月27日 03:45:29

--- app/src/main/java/com/pku/codingma/client/IndexActivity.java
+++ app/src/main/java/com/pku/codingma/client/IndexActivity.java
@@ -4,13 +4,25 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class IndexActivity extends AppCompatActivity {
 
+    private ImageView avatar;
+    private TextView username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+    }
+    @Override
+    protected void onStart(){
+        avatar=(ImageView) findViewById(R.id.avatar);
+        username=(TextView) findViewById(R.id.username);
+        //avatar.setImageResource(R.drawable.sex);
+        username.setText(StaticUsr.userName);
+        super.onStart();
     }
     public void toLogin(View v){
         Intent intent=new Intent(this, MainActivity.class);
